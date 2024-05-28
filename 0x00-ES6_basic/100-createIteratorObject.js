4824ad9c1b73aef8275e0611e0bb0c37819ecb08
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-05-28 04:37:14 +0000

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,10 @@
-export default function* createIteratorObject(report) {
-    const departments = Object.values(report.allEmployees);
-  
-    for (const employees of departments) {
-      for (const employee of employees) {
-        yield employee;
-      }
-    }
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...item,
+    ];
   }
+  return allEmployees;
+}
