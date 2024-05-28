cbf8c763ee4285800971f0650b25414127ce0878
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-05-28 04:31:37 +0000

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,18 +1,15 @@
 export function taskFirst() {
-    // Declare task as a constant because its value does not change
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  // Function to return a string
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    // Declare combination using let because its value will change
-    let combination = 'But sometimes let';
-    combination += getLast(); // Modify the value of combination
-  
-    return combination;
-  }
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
