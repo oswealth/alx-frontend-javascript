1da6621d7e5d85ad00abb6cf94419b3e5bd0629d
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-05-28 04:38:02 +0000

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-    const employees = [];
-  
-    for (const value of reportWithIterator) {
-      employees.push(value);
-    }
-  
-    return employees.join(' | ');
-  }  
+  return [...reportWithIterator].join(' | ');
+}
