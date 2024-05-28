fa6e3838579e9c2fd6c99e84b798667149c42110
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-05-28 03:08:13 +0000

    Delete 0x00-ES6_basic/2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++
@@ -1,9 +0,0 @@
-export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-  // Using an arrow function for addNeighborhood
-  this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
-  };
-}
