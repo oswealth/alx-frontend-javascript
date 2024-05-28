26990c8bf7e80058d16a3928d7b3a678cf8564bb
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-05-28 04:34:16 +0000

    Update 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-    return {
-      [departmentName]: employees,
-    };
-  }  
+  return { [departmentName]: [...employees] };
+}
