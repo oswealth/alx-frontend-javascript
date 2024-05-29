da938d6b3cc29601fc6bdb00031712697efde14e
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-05-29 20:36:31 +0000

    return a promise. The parameter is a boolean

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,9 @@
-export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-      if (success) {
-        resolve({
-          status: 200,
-          body: 'Success'
-        });
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
-  }  
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
