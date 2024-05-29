0d7ee2a114ed47e1fd579fdd1fa131ed5efb998a
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-05-29 20:30:37 +0000

    Return a Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve) => {
-      resolve('msg');
-    });
-  }
+  return new Promise((resolve) => {
+    resolve();
+  });
+}
