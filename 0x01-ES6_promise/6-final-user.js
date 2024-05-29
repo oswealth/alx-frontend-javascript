5668da2fdbbb6b23a158144666cb1214f8747439
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-05-29 20:46:38 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,25 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const newArray = [];
-  await signUpUser(firstName, lastName).then((user) => (
-    newArray.push({ status: 'fulfilled', value: user })));
-  await uploadPhoto(fileName).catch((error) => (
-    newArray.push({ status: 'rejected', value: error.toString() })));
-  return newArray;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)])
+    .then((result) => {
+      const array = [];
+      result.forEach((index) => {
+        if (index.status === 'fulfilled') {
+          array.push({
+            status: index.status,
+            value: index.value,
+          });
+        } else {
+          array.push({
+            status: index.status,
+            value: `Error: ${index.reason.message}`,
+          });
+        }
+      });
+      return array;
+    });
 }
