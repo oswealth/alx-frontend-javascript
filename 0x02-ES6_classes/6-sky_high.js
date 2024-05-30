366d3105f4f843aeb63081195a01967c1acf2cc9
oswealth <osbertjaywealth@gmail.com>
2024-05-30 19:47:02 +0000

    Implement a class named SkyHighBuilding that extends from Building

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,11 +1,9 @@
+
 import Building from './5-building';
 
 class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof floors !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
     this._floors = floors;
   }
 
@@ -14,7 +12,7 @@ class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
 
