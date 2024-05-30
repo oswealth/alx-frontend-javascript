2f7635959a51bd628afb59fac0caf66b7a00752d
oswealth <osbertjaywealth@gmail.com>
2024-05-30 19:46:36 +0000

    Implement a class named Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,15 @@
-export default class Building {
-  constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
-    }
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Square footage must be a number');
-    }
-    this._sqft = sqft;
 
-    if (this.evacuationWarningMessage === undefined) {
+class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 }
+
+export default Building;
